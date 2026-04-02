e8d46513a652d178996b78fec455e88ea2aa0084
Akshay Basutkar <akshaybasutkar.ai24@bmsce.ac.in>
2026-04-02 16:36:28 +0530

    Added

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,27 @@
-import Login from "./Login";
+import GalaxyBackground from "./components/GalaxyBackground";
 
 function App() {
-  return <Login />;
+  return (
+    <div style={{ width: "100%", height: "100vh", position: "relative" }}>
+      <GalaxyBackground />
+
+      <div
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          color: "white",
+        }}
+      >
+        <h1>Login Page</h1>
+      </div>
+    </div>
+  );
 }
 
 export default App;
